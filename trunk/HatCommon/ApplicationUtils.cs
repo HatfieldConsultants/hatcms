47fd47bf3186fccfe282e32903bc2d3790bae7f2
jsuwala@hatfieldgroup.com <jsuwala@hatfieldgroup.com@8bf6f626-2f36-9281-9cdb-d9b7190f63b6>
2011-06-02 23:35:32 +0000

    application error handler update: Don't send emails if debugging.

--- trunk/HatCommon/ApplicationUtils.cs
+++ trunk/HatCommon/ApplicationUtils.cs
@@ -11,6 +11,10 @@ namespace Hatfield.Web.Portal
     {
         public static void Application_Error_StandardEmailSender(HttpContext context, string fromEmailAddress, string[] toEmailAddresses, string smtpHostName)
         {
+            // don't do anything if we're debugging.
+            if (context.IsDebuggingEnabled)
+                return;
+
             string emailSubject = context.Request.Url.ToString().Split('/')[2] + " has an ASP.NET error at " + DateTime.Now.ToString("MMM dd yyyy HH:mm");
 
             System.Net.Mail.MailAddress from = new MailAddress(fromEmailAddress);
