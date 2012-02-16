4ac9da25c25f7fcde8a96300f7bdf7f93f00248f
luzhanxun.daniel@gmail.com <luzhanxun.daniel@gmail.com@8bf6f626-2f36-9281-9cdb-d9b7190f63b6>
2012-02-16 00:44:48 +0000

    rename servicelocatorinitializer

---
+++ HatCMS/trunk/HatCMS.Core/DatabaseServiceInitializer/DatabaseServiceLocatorInitializer.cs
@@ -0,0 +1,55 @@
+using Castle.Windsor;
+using SharpArch.Core.CommonValidator;
+using SharpArch.Data.NHibernate;
+using SharpArch.Core.PersistenceSupport;
+using SharpArch.Core.NHibernateValidator.CommonValidatorAdapter;
+using CommonServiceLocator.WindsorAdapter;
+using Microsoft.Practices.ServiceLocation;
+using HatCMS.Core.DataInterface;
+using HatCMS.Core.DataRepository;
+
+
+namespace HatCMS.Core
+{
+    using Castle.MicroKernel.Registration;
+
+    public class DatabaseServiceLocatorInitializer
+    {
+        public static void Init()
+        {
+            IWindsorContainer container = new WindsorContainer();
+            container.Register(
+                    Component
+                        .For(typeof(ISessionFactoryKeyProvider))
+                        .ImplementedBy(typeof(DefaultSessionFactoryKeyProvider))
+                        .Named("sessionFactoryKeyProvider"));
+
+            container.Register(
+                Component
+                    .For(typeof(IPersistenceVariableRepository))
+                    .ImplementedBy(typeof(PersistentVariableRepository))
+                    .Named("persistencevariableRepository"));
+
+            container.Register(
+                Component
+                    .For(typeof(IPageSecurityZoneUserRoleRepository))
+                    .ImplementedBy(typeof(PageSecurityZoneUserRoleRepository))
+                    .Named("pagesecurityzoneuserroleRepository"));
+
+            container.Register(
+                Component
+                    .For(typeof(IPageRepository))
+                    .ImplementedBy(typeof(PageRepository))
+                    .Named("pageRepository"));
+
+
+
+
+
+            ServiceLocator.SetLocatorProvider(delegate
+            {
+                return new WindsorServiceLocator(container);
+            });
+        }
+    }
+}
